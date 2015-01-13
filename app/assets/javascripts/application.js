e499fdfbc86ce9d733867a55299ce2fc2728aad8
Lovell McIlwain <lmcilwain@aspph.org>
2015-01-13 08:04:11 -0500

    add search to categories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require jquery.scrolly.min.js
 //= require jquery.scrollzer.min.js
