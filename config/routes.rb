929ebe9ee5372d6b5f355196e0dc3f019e137efa
Lovell McIlwain <lmcilwain@aspph.org>
2015-01-16 09:29:41 -0500

    finish show controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  
   root to: 'home#index'
-  resources :contact_me
+  
+  resources :contact_mes, only: [:index, :show, :create]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
