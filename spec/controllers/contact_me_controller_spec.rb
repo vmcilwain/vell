547e189ebb3039ae4b1d211ecbde2cda44dc8d35
Lovell McIlwain <lmcilwain@aspph.org>
2015-01-16 08:26:00 -0500

    add xhr test for creating contact me

--- spec/controllers/contact_me_controller_spec.rb
+++ spec/controllers/contact_me_controller_spec.rb
@@ -13,28 +13,54 @@ describe ContactMeController do
   end
   
   describe 'Post create' do
-    it 'sets @contact_me' do
-      post :create, contact_me: {email: Faker::Internet.email, name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
-      expect(assigns[:contact_me]).to be_instance_of ContactMe
+    context 'http request' do
+      it 'sets @contact_me' do
+        post :create, contact_me: {email: Faker::Internet.email, name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
+        expect(assigns[:contact_me]).to be_instance_of ContactMe
+      end
+      
+      describe 'successfully created contact me' do
+        before {post :create, contact_me: {email: Faker::Internet.email, name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
+  }
+        it 'sets flash[:success]' do
+          expect(flash[:success]).to_not be_nil
+        end
+        
+        it 'redirects to root path' do
+          expect(response).to redirect_to root_path
+        end
+      end
+      describe 'unsuccessfully created contact me' do
+        before {post :create, contact_me: {name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
+  }
+        it 'sets flash[:error]' do
+          expect(flash[:error]).to_not be_nil
+        end
+        
+        it 'redirects to root path' do
+          expect(response).to redirect_to root_path
+        end
+      end
     end
-    describe 'successfully contact me' do
-      before {post :create, contact_me: {email: Faker::Internet.email, name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
-}
-      it 'sets flash[:success]' do
-        expect(flash[:success]).to_not be_nil
+    
+    context 'xhr request' do
+      it 'sets @contact_me' do
+        xhr :post, :create, contact_me: {email: Faker::Internet.email, name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
+        expect(assigns[:contact_me]).to be_instance_of ContactMe
       end
-      it 'redirects to root path' do
-        expect(response).to redirect_to root_path
+      
+      describe 'successfully created contact me' do
+        it 'sets flash[:success]' do
+          xhr :post, :create, contact_me: {email: Faker::Internet.email, name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
+          expect(flash[:success]).to_not be_nil
+        end
       end
-    end
-    describe 'unsuccessfully created contact me' do
-      before {post :create, contact_me: {name: Faker::Name.name, body: Faker::Lorem.words(5).join("\s")}
-}
-      it 'sets flash[:error]' do
-        expect(flash[:error]).to_not be_nil
-      end
-      it 'redirects to root path' do
-        expect(response).to redirect_to root_path
+      
+      describe 'successfully created contact me' do
+        it 'sets flash[:success]' do
+          xhr :post, :create, contact_me: {email: Faker::Internet.email, body: Faker::Lorem.words(5).join("\s")}
+          expect(flash[:error]).to_not be_nil
+        end
       end
     end
   end
