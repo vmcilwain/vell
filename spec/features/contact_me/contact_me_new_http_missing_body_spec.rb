9d3606b981f7683ccf7a5e29257082bb3a071ddc
Vell <vmcilwain@me.com>
2015-01-20 21:14:18 -0500

    added multiple feature test for xhr contact me submission

--- spec/features/contact_me/contact_me_new_http_missing_body_spec.rb
+++ spec/features/contact_me/contact_me_new_http_missing_body_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-feature 'create contact me with missing name' do
+feature 'create contact me with missing body' do
   scenario 'user unsuccessfuly creates contact me via http' do
     visit root_path
 
     fill_in 'contact_me_name', with: 'John Rambo'
-    fill_in 'contact_me_email', with: 'jrambo@example.com'
+    fill_in 'contact_me_body', with: 'This is the body of a contact me'
     click_button 'Send Message'
         
     expect(page).to have_content "There was a problem with your message."
-    expect(page).to have_content "Body can't be blank"
+    expect(page).to have_content "Email can't be blank"
   end
 end
