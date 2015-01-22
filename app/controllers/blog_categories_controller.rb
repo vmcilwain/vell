f419c1379aa71b7fc32558ecd496c11bfa898376
Vell <vmcilwain@me.com>
2015-01-21 21:23:49 -0500

    add create controller test

--- app/controllers/blog_categories_controller.rb
+++ app/controllers/blog_categories_controller.rb
@@ -2,4 +2,25 @@ class BlogCategoriesController < ApplicationController
   def index
     @blog_categories = BlogCategory.all
   end
+  
+  def new
+    @blog_category = BlogCategory.new
+  end
+  
+  def create
+    @blog_category = BlogCategory.new(blog_category_params)
+    if @blog_category.save
+      flash[:success] = "Blog Category has been created!"
+      redirect_to blog_categories_path
+    else
+      flash[:error] = "There was a problem!"
+      render :new
+    end
+  end
+  
+  private
+  
+  def blog_category_params
+    params.require(:blog_category).permit(:name)
+  end
 end
--- spec/controllers/blog_categories_controller_spec.rb
+++ spec/controllers/blog_categories_controller_spec.rb
@@ -10,8 +10,44 @@ describe BlogCategoriesController do
     end
   end
   
-  describe "GET new"
-  describe "POST create"
+  describe "GET new" do
+    it 'sets @blog_category' do
+      get :new
+      expect(assigns[:blog_category]).to be_instance_of BlogCategory
+    end
+  end
+  
+  describe "POST create" do    
+    it 'sets @blog_category' do
+      post :create, blog_category: {name: Faker::Lorem.words(1).first}
+      expect(assigns[:blog_category]).to be_instance_of BlogCategory
+    end
+    
+    context 'a successful creation' do
+      it 'redirects to :index' do
+        post :create, blog_category: {name: Faker::Lorem.words(1).first}
+        expect(response).to redirect_to blog_categories_path
+      end
+      
+      it 'sets flash[:success]' do
+        post :create, blog_category: {name: Faker::Lorem.words(1).first}
+        expect(flash[:success]).to_not be_nil
+      end
+    end
+    
+    context 'an unsuccesful creation' do
+      it 'renders :new' do
+        post :create, blog_category: {name: ''}
+        expect(response).to render_template :new
+      end
+      
+      it 'sets flash[:error]' do
+        post :create, blog_category: {name: ''}
+        expect(flash[:error]).to_not be_nil
+      end
+    end
+  end
+  
   describe "GET edit"
   describe "PUT update"
   describe 'DELETE destroy'
