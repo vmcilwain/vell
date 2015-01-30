b02e70b294541ad7c03d7e85bf5b64d474699a92
Vell <vmcilwain@me.com>
2015-01-30 09:55:59 -0500

    add cancel new blog category feature

---
+++ spec/features/blog_category/blog_category_new_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'creating a blog category' do
+  scenario 'administrator should be able to create a blog category' do
+    visit blog_categories_path
+    click_link 'New Category'
+
+    expect(page).to have_content 'Create Blog Category'
+
+    fill_in 'blog_category_name', with: 'Ruby On Rails'
+    click_button 'Create'
+
+    expect(page).to have_content 'Blog Category has been created!'
+  end
+end
