5c5f61550b87b1c44377a23389f41d53ed5ee578
Vell <vmcilwain@me.com>
2015-02-02 22:37:30 -0500

    add update and destroy tests

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :blog, only: [:edit, :update, :destroy]
+  
   def index
     @blogs = Blog.all
   end
@@ -18,7 +20,27 @@ class BlogsController < ApplicationController
     end
   end
   
+  def update
+    if @blog.update(blog_params)
+      flash[:success] = 'Blog updated!'
+      redirect_to @blog
+    else
+      flash[:error] = 'There was a problem!'
+      render :new
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    flash[:success] = 'Blog deleted!'
+    redirect_to blogs_path
+  end
+  
   private
+  
+  def blog
+    @blog = Blog.find(params[:id])
+  end
   
   def blog_params
     params.require(:blog).permit(:blog_category_id, :headline, :body)
--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -50,7 +50,66 @@ describe BlogsController do
     end
   end
   
-  describe 'GET edit'
-  describe 'PUT update'
-  describe 'DELETE destroy'
+  describe 'GET edit' do
+    let(:blog) {Fabricate :blog}
+    it 'sets @blog' do
+      get :edit, id: blog.id
+      expect(assigns[:blog]).to eq blog
+    end
+  end
+  
+  describe 'PUT update' do
+    let(:blog) {Fabricate :blog}
+    context 'a succesful update' do
+      before{put :update, id: blog.id, blog: {headline: Faker::Lorem.words(5).join("\s"), blog_category_id: 1, body: Faker::Lorem.words(25).join("\s")}}
+      
+      it 'redirects to :show' do
+        expect(response).to redirect_to blog_path(blog)
+      end
+      
+      it 'sets @blog' do
+        expect(assigns[:blog]).to eq blog
+      end
+      
+      it 'sets flash[success]' do
+        expect(flash[:success]).to_not be_nil
+      end
+    end
+    
+    context 'an unsuccessful update' do
+      before{put :update, id: blog.id, blog: {headline: Faker::Lorem.words(5).join("\s"), blog_category_id: 1, body: ""}}
+      
+      it 'renders :new' do
+        expect(response).to render_template :new
+      end
+      
+      it 'sets @blog' do
+        expect(assigns[:blog]).to eq blog
+      end
+      
+      it 'sets flash[:error]' do
+        expect(flash[:error]).to_not be_nil
+      end
+    end
+  end
+  
+  describe 'DELETE destroy' do
+    let(:blog) {Fabricate :blog}
+    before{delete :destroy, id: blog.id}
+    it 'sets @blog' do
+      expect(assigns[:blog]).to eq blog
+    end
+    
+    it 'destroys @blog' do
+      expect(Blog.where(id: blog.id)).to eq []
+    end
+    
+    it 'redirects to :index' do
+      expect(response).to redirect_to blogs_path
+    end
+    
+    it 'sets flash[:success]' do
+      expect(flash[:success]).to_not be_nil
+    end
+  end
 end
