3c9e10708439d0138cada5094a09f57518c099e4
Vell <vmcilwain@me.com>
2015-02-03 20:50:18 -0500

    add blog show feature

--- spec/support/objects.rb
+++ spec/support/objects.rb
@@ -3,9 +3,9 @@ def contact_me
 end
 
 def blog_category
-  @blog_category || Fabricate(:blog_category)
+  @blog_category ||= Fabricate(:blog_category)
 end
 
 def blog
-  @blog || Fabricate(:blog)
+  @blog ||= Fabricate(:blog)
 end
