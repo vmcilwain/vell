d2aceaa9050cab89b84c03ba9011fc91d076edc3
Vell <vmcilwain@me.com>
2015-02-21 14:09:53 -0500

    add blog show download attachment feature

---
+++ spec/features/blog_file/blog_file_download_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'downloading a blog file' do
+  scenario 'user can download a file from file listing' do
+    blog_file
+    visit blog_files_path
+    expect(page).to have_link 'Download'
+    click_link 'Download'
+    expect(page.response_headers['Content-Type']).to eq('text/plain')
+  end
+end
