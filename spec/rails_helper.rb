c067cdb4a61abbaa49735e42232511b1f9813b1f
Vell <vmcilwain@me.com>
2015-02-22 20:12:09 -0500

    checking cassette ignore

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,10 +9,10 @@ require 'pry'
 
 require 'vcr'
 VCR.configure do |c|
-c.cassette_library_dir = 'spec/cassettes'
-c.hook_into :webmock
-c.configure_rspec_metadata!
-c.ignore_localhost = true
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.ignore_localhost = true
 end
 # Add additional requires below this line. Rails is not loaded until this point!
 
