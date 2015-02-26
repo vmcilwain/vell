d7b0796d25cc2912ab1a66072d949450fe9a83b8
Vell <vmcilwain@me.com>
2015-02-25 21:25:43 -0500

    add create test

--- app/controllers/photo_albums_controller.rb
+++ app/controllers/photo_albums_controller.rb
@@ -9,9 +9,24 @@ class PhotoAlbumsController < ApplicationController
     @photo_album = PhotoAlbum.new
   end
   
+  def create
+    @photo_album = PhotoAlbum.new(photo_album_params)
+    if @photo_album.save
+      flash[:success] = 'Photo album created'
+      redirect_to @photo_album
+    else
+      flash[:error] = 'There were errors'
+      render :new
+    end
+  end
+  
   private
   
   def photo_album
     @photo_album = PhotoAlbum.find(params[:id])
+  end
+  
+  def photo_album_params
+    params.require(:photo_album).permit(:name, :description)
   end
 end
