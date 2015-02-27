9e58b3633876f0af53e6133f15447eb95198c7f6
Vell <vmcilwain@me.com>
2015-02-27 15:19:41 -0500

    create new valid and invalid feature test

---
+++ spec/features/photo_album/photo_album_valid_create_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'photo albums' do
+  scenario 'creating a photo album' do
+    visit photo_albums_path
+    expect(page).to have_text 'Photo Albums'
+    
+    click_link 'New Album'
+    fill_in 'photo_album_name', with: Faker::Lorem.words(1).first
+    fill_in 'photo_album_description', with: Faker::Lorem.words(5).join("\s")
+    click_button 'Create Photo album'
+    expect(page).to have_content 'Photo album created'
+  end
+end
