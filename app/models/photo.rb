a6188b877245262477e81b364b24dedddf7f95d2
Vell <vmcilwain@me.com>
2015-03-01 12:06:06 -0500

    add photo model spec tests

---
+++ app/models/photo.rb
@@ -0,0 +1,6 @@
+class Photo < ActiveRecord::Base
+  belongs_to :photo_album
+  validates :photo_album_id, :name, presence: true
+  has_attached_file :document, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :document, :content_type => /\Aimage\/.*\Z/
+end
