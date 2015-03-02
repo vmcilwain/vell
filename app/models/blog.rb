176dd7848bc4a10f8b277aabeb39f0d1c4ca6fca
Vell <vmcilwain@me.com>
2015-03-02 12:00:19 -0500

    added validates associated to models and added photo comment test

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,12 @@
 class Blog < ActiveRecord::Base
   validates :headline, :blog_category_id, :body, presence: true
+  
   belongs_to :blog_category
+  
   has_many :blog_files
+  
   has_many :blog_comments
+  
   accepts_nested_attributes_for :blog_files, allow_destroy: true
+  
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,4 +7,8 @@ class Photo < ActiveRecord::Base
   has_attached_file :document, :styles => { :medium => "300x300>", :thumb => "100x100>" }#, :default_url => "/images/:style/missing.png"
   
   validates_attachment_content_type :document, :content_type => ALLOWABLE_IMAGES
+  
+  has_many :photo_comments
+  
+  validates_associated :photo_comments
 end
