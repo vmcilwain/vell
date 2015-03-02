71aa29f8833eceaa3af82dce5fe3904fdf5b52a2
Vell <vmcilwain@me.com>
2015-03-02 12:54:54 -0500

    add create controller test

--- app/controllers/photo_comments_controller.rb
+++ app/controllers/photo_comments_controller.rb
@@ -8,9 +8,24 @@ class PhotoCommentsController < ApplicationController
     @photo_comment = PhotoComment.new
   end
   
+  def create
+    @photo_comment = PhotoComment.new(photo_comment_params)
+    if @photo_comment.save
+      flash[:success] = 'Photo comment created'
+      redirect_to @photo_comment.photo
+    else
+      flash[:error] = 'There were errors'
+      render :new
+    end
+  end
+  
   private
   
   def photo_comment
     @photo_comment = PhotoComment.find(params[:id])
+  end
+  
+  def photo_comment_params
+    params.require(:photo_comment).permit(:photo_id, :name, :headline, :body)
   end
 end
