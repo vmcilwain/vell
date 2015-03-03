58fcfb61862d70c64fabf8d81129198e5e85f436
Vell <vmcilwain@me.com>
2015-03-02 22:50:57 -0500

    fix blog ui to adapt to skel structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # Produces -> Thursday 25 May 2006 - 1:08 PM
+  def long_date(date)
+    h date.strftime("%A %d %B %Y - %H:%M %p")
+  end
+
+  # Produces -> 2009-01-26
+  def medium_date(date)
+    h date.strftime("%Y-%m-%d - %H:%M %p")
+  end
+
+  # Produces -> 2009-01-26
+  def short_date(date)
+    h date.strftime("%Y-%m-%d") rescue ''
+  end
 end
