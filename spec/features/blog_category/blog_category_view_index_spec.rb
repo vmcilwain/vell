3eb7051f75d4781ac1f6c93c26971de100aeeaaf
Vell <vmcilwain@me.com>
2015-03-04 22:52:02 -0500

    more tweaks to the ui

--- spec/features/blog_category/blog_category_view_index_spec.rb
+++ spec/features/blog_category/blog_category_view_index_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 feature 'viewing blog category index' do
   scenario 'view list of blog categories' do
-    visit '/'
-    
-    click_link 'Blog Categories'
-    
+    visit blog_categories_path    
     expect(page).to have_content 'Blog Categories'
   end
 end
