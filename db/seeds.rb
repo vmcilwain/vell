1d2bdf3ec46e31a12b03a479934928db70977deb
Lovell McIlwain <lmcilwain@aspph.org>
2015-03-06 09:22:15 -0500

    add file path for file uploads to s3 and created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ContactMe.create([{name: Faker::Name.name, email: Faker::Internet.email, body: Faker::Lorem.words(50).join("\s")},
+                  {name: Faker::Name.name, email: Faker::Internet.email, body: Faker::Lorem.words(50).join("\s")},
+                  {name: Faker::Name.name, email: Faker::Internet.email, body: Faker::Lorem.words(50).join("\s")},
+                  {name: Faker::Name.name, email: Faker::Internet.email, body: Faker::Lorem.words(50).join("\s")},
+                  {name: Faker::Name.name, email: Faker::Internet.email, body: Faker::Lorem.words(50).join("\s")}])
+                  
+blog_categories = BlogCategory.create([{name: Faker::Lorem.words(1).first},
+                                       {name: Faker::Lorem.words(1).first},
+                                       {name: Faker::Lorem.words(1).first},
+                                       {name: Faker::Lorem.words(1).first},
+                                       {name: Faker::Lorem.words(1).first}])
+
+Blog.create([{blog_category_id: blog_categories.shuffle.sample(1).first.id, headline: Faker::Lorem.words(5).join("\s"), body: Faker::Lorem.words(50).join("\s")},
+                     {blog_category_id: blog_categories.shuffle.sample(1).first.id, headline: Faker::Lorem.words(5).join("\s"), body: Faker::Lorem.words(50).join("\s")},
+                     {blog_category_id: blog_categories.shuffle.sample(1).first.id, headline: Faker::Lorem.words(5).join("\s"), body: Faker::Lorem.words(50).join("\s")},
+                     {blog_category_id: blog_categories.shuffle.sample(1).first.id, headline: Faker::Lorem.words(5).join("\s"), body: Faker::Lorem.words(50).join("\s")},
+                     {blog_category_id: blog_categories.shuffle.sample(1).first.id, headline: Faker::Lorem.words(5).join("\s"), body: Faker::Lorem.words(50).join("\s")}])
+
+Blog.all.each do |blog|
+  5.times {BlogComment.create(blog_id: blog.id, name: Faker::Name.name, body: Faker::Lorem.words(50).join("\s"))}
+end
+
+photo_albums = PhotoAlbum.create([{name: Faker::Lorem.words(1).first, description: Faker::Lorem.words(20).join("\s")},
+                                  {name: Faker::Lorem.words(1).first, description: Faker::Lorem.words(20).join("\s")},
+                                {name: Faker::Lorem.words(1).first, description: Faker::Lorem.words(20).join("\s")},
+                                {name: Faker::Lorem.words(1).first, description: Faker::Lorem.words(20).join("\s")},
+                                {name: Faker::Lorem.words(1).first, description: Faker::Lorem.words(20).join("\s")}])
+
+if ENV['PHOTOS']                      
+  photo_albums.each do |photo_album|
+    5.times{Photo.create(photo_album_id: photo_album.id, description: Faker::Lorem.words(5).join("\s"), document: File.open("#{Rails.root.to_s}/spec/support/files/test_picture.jpg"))}
+  end
+end
+
+puts "FIN!"
