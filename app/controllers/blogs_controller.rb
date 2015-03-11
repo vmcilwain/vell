d289b27e69aac833f9b16e43153bb885f0fae8f4
Vell <vmcilwain@me.com>
2015-03-10 21:57:13 -0400

    updated twitter message

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,8 +17,8 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
-      text = "I created a new blog entry. You can see it here: #{GoogleUrlShortenerService.new('http://vell.herokuapp.com/blogs/#{@blog.id}').short_url}"
-      $twitter.update(text) unless Rails.env.development?
+      url = GoogleUrlShortenerService.new("http://vell.herokuapp.com/blogs/#{@blog.id}").short_url
+      $twitter.update("I created a new blog entry. You can see it here: " + url) unless Rails.env.development?
       flash[:success] = 'Blog created!'
       redirect_to @blog
     else
