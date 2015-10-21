54e610663227f9012c2c277b6fa2c3a534206d68
Vell <vmcilwain@me.com>
2015-10-21 19:33:50 -0400

    fix blog file feature

--- spec/features/blog_file/blog_file_index_spec.rb
+++ spec/features/blog_file/blog_file_index_spec.rb
@@ -5,8 +5,11 @@ feature 'blog file index' do
   scenario 'viewing blog file index' do
     blog_file1 = Fabricate :blog_file
     blog_file2 = Fabricate :blog_file
-    sign_in
+    BlogFile.reindex
     visit blog_files_path
+    
+    sign_in_with(admin_user)
+    
 
     expect(page.all('a', text: 'test_file.txt').size).to eq 2
   end
