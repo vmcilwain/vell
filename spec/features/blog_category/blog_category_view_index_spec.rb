37dbd256f627f1a616af51b765d140d9a9478676
Vell <vmcilwain@me.com>
2016-02-15 15:42:18 -0500

    remove blog category specs and files

--- spec/features/blog_category/blog_category_view_index_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature 'blog category' do
-  scenario 'view list of blog categories' do
-    visit blog_categories_path
-    sign_in_with(admin_user)
-    expect(page).to have_content 'Blog Categories'
-  end
-end
