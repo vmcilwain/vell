e45278c201025befae7e14e0f621f30bfedbaf16
Vell <vmcilwain@me.com>
2016-02-15 17:53:57 -0500

    remove email field tests for contact me; add flash to index page

--- spec/features/contact_me/contact_me_show_spec.rb
+++ spec/features/contact_me/contact_me_show_spec.rb
@@ -7,9 +7,8 @@ feature 'viewing contact me' do
     
     sign_in_with(admin_user)
     
-    expect(page).to have_link contact_me.id
+    click_link contact_me.name
     
-    click_link contact_me.id
     expect(page).to have_text "this is a new contact me!"
     
     clear_mailbox      
