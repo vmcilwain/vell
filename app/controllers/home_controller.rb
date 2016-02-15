83eaf47bc80b1247ab0bdff79c93a93c32f6ac34
Vell <vmcilwain@me.com>
2016-02-14 19:01:08 -0500

    move index ui to production

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,5 @@
 class HomeController < ApplicationController
-  # before_action :home, only: [:edit, :update]
- #  before_action :require_user, except: [:index, :about]
- #  before_action :require_admin, except: [:index, :about]
- #  def index
- #    @home = Home.first
- #  end
- #
- #  def about
- #    @home = Home.first
- #  end
- #
- #  def new
- #    @home = Home.new
- #  end
- #
- #
- #  def create
- #    @home = Home.new(home_params)
- #    @home.save
- #    flash[:success] = 'Home info updated'
- #    redirect_to root_path
- #  end
- #  private
- #
- #  def home
- #    @home = Home.find(params[:id])
- #  end
- #
- #  def home_params
- #    params.require(:home).permit(:index, :about)
- #  end
+  def index
+    @blogs = Blog.last(4)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,22 @@
 module ApplicationHelper
   # Produces -> Thursday 25 May 2006 - 1:08 PM
   def long_date(date)
-    h date.strftime("%A %d %B %Y - %H:%M %p")
+    date.strftime("%A %d %B %Y - %H:%M %p")
   end
 
   # Produces -> 2009-01-26
   def medium_date(date)
-    h date.strftime("%Y-%m-%d - %H:%M %p")
+    date.strftime("%Y-%m-%d - %H:%M %p")
+  end
+  
+  #Produces -< 18 October 2015
+  def medium_date2(date)
+    date.strftime("%d %B %Y")
   end
 
   # Produces -> 2009-01-26
   def short_date(date)
-    h date.strftime("%Y-%m-%d") rescue ''
+    date.strftime("%Y-%m-%d") rescue ''
   end
   
   def to_text(text)
