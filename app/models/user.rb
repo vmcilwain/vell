b5d9cd25fb944313ee5efd57a0f66bb15519ac81
Vell <vmcilwain@me.com>
2016-02-15 16:35:31 -0500

    style devise views and remove registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable#, :registerable
    # validates :first_name, :last_name, presence: true
    def full_name
      "#{self.first_name} #{self.last_name}"
