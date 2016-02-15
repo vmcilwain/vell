d200b0cc9a9f0d272f0ae33946c473b0f18934f7
Vell <vmcilwain@me.com>
2016-02-15 18:22:53 -0500

    update mailer default

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default to: 'Vell <vmcilwain@me.com>'
-  default from: 'vell.herokuapp.com <vmcilwain@me.com>'
+  default from: 'lovell.direct <vmcilwain@me.com>'
   helper :application
   layout 'mailer'
 end
