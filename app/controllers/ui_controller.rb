55450bc498f8ba859a466ff61f089611687f4941
Vell <vmcilwain@me.com>
2016-02-18 18:34:57 -0500

    update documenation

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,4 +1,7 @@
+# @author Lovell McIlwain
+# Handles HTTP actions for UI
 class UiController < ApplicationController
+  # Before filter to check if application is running in development
    before_action do
     redirect_to :root if Rails.env.production?
   end
