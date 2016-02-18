3231ab7941bcbe2b82e308acce0da9710916b938
Vell <vmcilwain@me.com>
2016-02-18 18:33:11 -0500

    update documentatiopn

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
+# @author Lovell McIlwain
+# Handles HTTP actions for home
 class HomeController < ApplicationController
+  # Handles HTTP get
+  #
+  # @return [Array] the last 4 blogs created
   def index
     @blogs = Blog.last(4)
   end
