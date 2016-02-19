00a643931020c72e0f84abe69dccb6fedf9e63da
Vell <vmcilwain@me.com>
2016-02-18 19:05:43 -0500

    update documenation

--- app/models/contact_me.rb
+++ app/models/contact_me.rb
@@ -1,10 +1,17 @@
+# @author Lovell McIlwain
+# Handles business logic for contact me
 class ContactMe < ActiveRecord::Base
+  # Used with elasticsearch
   searchkick
   
+  # ActiveRecord Validation to ensure name, email and body are present on submission
   validates :name, :email, :body, presence: true
   
+  # ActiveRecord Callback
+  # (see #reindex_blog)
   after_save :reindex_blog
   
+  # Searchkick index fields
   def search_data
     {
       name: name,
@@ -15,7 +22,8 @@ class ContactMe < ActiveRecord::Base
   end
 
   private
-
+  
+  # Rebuild search index
   def reindex_blog
     ContactMe.reindex # or reindex_async
   end
