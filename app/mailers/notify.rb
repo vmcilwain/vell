0e3b03afdf8e749b4c43052a424ddc521c475f4f
Vell <vmcilwain@me.com>
2016-02-18 19:15:20 -0500

    add documentation

--- app/mailers/notify.rb
+++ app/mailers/notify.rb
@@ -1,10 +1,18 @@
+# @author Lovell McIlwain
+# Handles notification logic
 class Notify < ApplicationMailer
+  # Send email with contact me info
+  #
+  # @param contact_me [ContactMe] the created contact me
   def send_contact_me(contact_me)
     @contact_me = contact_me
     mail(to: contact_me.email, subject: "New Contact Me From #{contact_me.name}")
     mail(subject: "New Contact Me From #{contact_me.name}")
   end
   
+  # Send email with blog comment info
+  #
+  # @param blog_comment [BlogComment] the created blog comment
   def send_new_blog_comment(blog_comment)
     @blog = blog_comment.blog
     @blog_comment = blog_comment
