a53f9a11d2b6789b4dfeabdabf471c3626dfbf7a
Vell <vmcilwain@me.com>
2016-02-18 19:03:28 -0500

    update documenation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,22 +1,38 @@
+# @author Lovell McIlwain
+# Handles business logic for blog
 class Blog < ActiveRecord::Base
+  # Used with elasticsearch
   searchkick
-
+  
+  # ActiveRecord validation to ensure headline and body are present
   validates :headline, :body, presence: true
-
+  
+  # ActiveRecord Association to child records
   has_many :blog_files
-
+  
+  # ActiveRecord Association to child records
   has_many :blog_comments
-
+  
+  ActiveRecord Callback
+  # (see #reindex_blog)
   after_save :reindex_blog
-
+  
+  # Accept attribtues for blog files to be created
   accepts_nested_attributes_for :blog_files, allow_destroy: true
-
+  
+  # Used with acts_as_taggable
   acts_as_taggable
+  
+  # Used with acts_as_taggable
   acts_as_taggable_on :announcements, :ruby, :rubyonrails, :linux, :General
   
+  # Allow blog to to use friendly ids instead of ids
   extend FriendlyId
+  
+  # Use headline as the friendly id
   friendly_id :headline, use: :slugged
   
+  # Searchkick index fields
   def search_data
     {
       headline: headline,
@@ -28,7 +44,8 @@ class Blog < ActiveRecord::Base
   end
 
   private
-
+  
+  # Rebuild search index
   def reindex_blog
     Blog.reindex # or reindex_async
   end
--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -1,13 +1,20 @@
+# @author Lovell McIlwain
+# Handles business logic for blog comment
 class BlogComment < ActiveRecord::Base
+  # Used with elasticsearch
   searchkick
-
+  
+  # ActiveRecord Association to parent record
   belongs_to :blog
-
+  
+  # ActiveRecord validation to ensure parent id and commenter name is present
   validates :blog_id, :name, :body, presence: true
-
+  
+  # ActiveRecord callback to run method after object is saved
   after_save :reindex_blog_comment
 
   private
+  # Rebuild search index
   def reindex_blog_comment
     BlogComment.reindex # or reindex_async
   end
