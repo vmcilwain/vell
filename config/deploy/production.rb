58b26f3e63a4da8dac8eaa089dacfb6e7bcdc18d
Vell <vmcilwain@me.com>
2016-03-03 19:07:16 -0500

    update ip address for production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 
-server '45.55.204.48', user: 'deploy', roles: %w{app db web}
+server '192.34.63.17', user: 'deploy', roles: %w{app db web}
 
 
 
