8b0ec89061ecd4c13c89d96c2e507137a68f8ebb
Vell <vmcilwain@me.com>
2016-03-14 21:18:32 -0400

    add update feature

---
+++ spec/features/projects/edit_project_path_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'creating a project' do
+  scenario 'a person can create a project if they have authenticated' do
+    project
+
+    visit root_path
+
+    click_link 'Login'
+    fill_in 'user_email', with: admin_user.email
+    fill_in 'user_password', with: 'somepassword'
+    click_button 'Log in'
+
+    click_link 'Projects'
+    click_link project.repo_url
+
+    click_link 'Edit'
+
+    fill_in 'project_repo_url', with: Faker::Internet.url
+    fill_in 'project_description', with: Faker::Lorem.words(5).join("\s")
+    click_button 'Update Project'
+
+    expect(page).to have_content 'Project updated'
+  end
+end
