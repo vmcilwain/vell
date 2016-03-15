8dbe43cc79e80c50715dff52ea4ed89a0ed190bb
Vell <vmcilwain@me.com>
2016-03-14 23:03:42 -0400

    fix blog file features

--- spec/features/blog_file/blog_file_destroy_spec.rb
+++ spec/features/blog_file/blog_file_destroy_spec.rb
@@ -7,7 +7,7 @@ feature 'blog file index' do
     visit blog_files_path
     sign_in_with(admin_user)
     expect(page).to have_link 'Delete'
-    
+
     click_link 'Delete'
     expect(page).to_not have_content blog_file.doc_file_name
   end
--- spec/features/blog_file/blog_file_index_spec.rb
+++ spec/features/blog_file/blog_file_index_spec.rb
@@ -4,12 +4,11 @@ feature 'blog file index' do
   after {delete_files}
   scenario 'viewing blog file index' do
     blog_file1 = Fabricate :blog_file
-    blog_file2 = Fabricate :blog_file
+
     visit blog_files_path
 
     sign_in_with(admin_user)
 
-
-    expect(page.all('a', text: 'test_file.txt').size).to eq 2
+    expect(page).to have_link 'test_file.txt'
   end
 end
