10346b6053614a6760f51603dc0dd3e0263fcf5d
Vell <vmcilwain@me.com>
2016-10-28 19:13:37 -0400

    move some files to config directory and fix broken contact me specs

--- spec/features/contact_me/contact_me_new_missing_name_spec.rb
+++ spec/features/contact_me/contact_me_new_missing_name_spec.rb
@@ -6,12 +6,12 @@ feature 'create contact me with missing name' do
 
     click_link 'Contact'
 
-    within '.well' do
+    within '#new_contact_me', match: :first do
       fill_in 'contact_me_email', with: 'jrambo@example.com'
       fill_in 'contact_me_body', with: 'This is the body of a contact me'
       click_button 'Send'
     end
-    
+
     expect(page).to have_content "Name can't be blank"
   end
 end
