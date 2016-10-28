49ed9e58bc16b96f9717b32d72da26256287e1bd
Vell <vmcilwain@me.com>
2016-10-28 18:53:48 -0400

    remove sign in method in favor of set session

--- spec/features/blog_file/blog_file_destroy_spec.rb
+++ spec/features/blog_file/blog_file_destroy_spec.rb
@@ -4,10 +4,8 @@ feature 'blog file index' do
   before {blog_file}
   after {delete_files}
   scenario 'viewing blog file index' do
+    set_session admin_user
     visit blog_files_path
-    sign_in_with(admin_user)
-    expect(page).to have_link 'Delete'
-
     click_link 'Delete'
     expect(page).to_not have_content blog_file.doc_file_name
   end
