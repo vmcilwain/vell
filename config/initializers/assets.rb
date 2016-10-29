239dd102c68998c4b39ccdbcb9fcf917e35d6993
Vell <vmcilwain@me.com>
2016-10-29 13:37:29 -0400

    add static file serving

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( animate.css blog.css font-awesome.css footer-distributed-with-contact-form.css readable.css tagcloud.css will_paginate.css)
+# Rails.application.config.assets.precompile += %w( animate.css blog.css font-awesome.css footer-distributed-with-contact-form.css readable.css tagcloud.css will_paginate.css)
