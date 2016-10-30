a4dc7f3406615e191dbe954837d6dbefe5936da4
Vell <vmcilwain@me.com>
2016-10-30 17:42:10 -0400

    add presenter class

---
+++ app/presenters/contact_me_presenter.rb
@@ -0,0 +1,36 @@
+# @author Lovell McIlwain
+#
+# Presenter class for object views
+class ContactMePresenter < BasePresenter
+  # Reference initialized object_presenter as object
+  presents :contact_me
+
+  delegate :id, to: :contact_me
+  delegate :body, to: :contact_me
+  delegate :name, to: :contact_me
+
+  # Return name as a link
+  def name_link
+    link_to contact_me.name, contact_me
+  end
+
+  # Return email as link
+  def email_link
+    mail_to contact_me.email
+  end
+
+  # Returns truncated body
+  def truncated_body
+    truncate(contact_me.body, length: 100) { link_to "Read More", contact_me }
+  end
+
+  # Returns medium created at
+  def medium_created_at
+    medium_date2 contact_me.created_at
+  end
+
+  # Returns destroy path as a button
+  def delete_button
+    link_to :Delete, contact_me, method: :delete, data: {confirm: "Are you sure?"}, class: 'btn btn-danger btn-xs'
+  end
+end
---
+++ spec/presenters/contact_me_presenter_spec.rb
@@ -0,0 +1,29 @@
+
+require 'rails_helper'
+#
+describe ContactMePresenter do
+  include_behavior
+
+  let(:presenter) {ContactMePresenter.new(contact_me, view)}
+  let(:contact_me) {Fabricate :contact_me}
+
+  it 'returns name as a link' do
+    expect(presenter.name_link).to eq link_to contact_me.name, contact_me
+  end
+
+  it "returns email as a link" do
+    expect(presenter.email_link).to eq mail_to contact_me.email
+  end
+
+  it "returns a truncated body" do
+    expect(presenter.truncated_body).to eq truncate(contact_me.body, length: 100) { link_to "Read More", contact_me }
+  end
+
+  it "returns medium created at date" do
+    expect(presenter.medium_created_at).to eq contact_me.created_at.strftime("%d %B %Y")
+  end
+
+  it "returns destroy path as a button" do
+    expect(presenter.delete_button).to eq link_to :Delete, contact_me, method: :delete, data: {confirm: "Are you sure?"}, class: 'btn btn-danger btn-xs'
+  end
+end
