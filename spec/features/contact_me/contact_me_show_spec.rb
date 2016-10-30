9b7c91deb349fd830c33ff14c408d9023a33f21f
Vell <vmcilwain@me.com>
2016-10-30 17:42:36 -0400

    switch from manual sign in to setting a session

--- spec/features/contact_me/contact_me_show_spec.rb
+++ spec/features/contact_me/contact_me_show_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 feature 'viewing contact me' do
   scenario "user views a contact me" do
     contact_me = Fabricate(:contact_me, name: 'John Rambo', email: 'jrambo@example.com', body: "this is a new contact me!")
+    set_session admin_user
+
     visit contact_mes_path
     
-    sign_in_with(admin_user)
-    
     click_link contact_me.name
-    
+
     expect(page).to have_text "this is a new contact me!"
-    
-    clear_mailbox      
+
+    clear_mailbox
   end
 end
