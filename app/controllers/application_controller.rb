0846f10cd4e4acb2f321009db7e19aadf447179b
Vell <vmcilwain@me.com>
2016-10-31 20:59:34 -0400

    add pundit rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,20 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
 
+  # Rescue from pundit error
+  # (see #user_not_authorized)
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   private
 
   def error_message
     "There were errors!"
+  end
+
+  # Method to gracefully let a user know they are are not authorized
+  #
+  # @return flash [Hash] the action notice
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to root_path
   end
 end
